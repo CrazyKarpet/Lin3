ebf794a1e34455dd142fa2fe4092683def32d25d
davidroulet <47849716+davidroulet@users.noreply.github.com>
2024-01-11 11:25:59 +0100

    Update kerberos_install.sh

--- Kerberos/kerberos_install.sh
+++ Kerberos/kerberos_install.sh
@@ -2,7 +2,7 @@
 cat << EOF > /etc/hosts
 127.0.0.1   localhost localhost.localdomain localhost4 localhost4.localdomain4 centos9-ks-01.lin3.local centos9-ks-01 lin3.local
 ::1         localhost localhost.localdomain localhost6 localhost6.localdomain6
-blabla
+
 EOF
 
 systemctl start firewalld
